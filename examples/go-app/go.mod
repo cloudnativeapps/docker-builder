040eb385c0d223884210b67ffb625cc49417dae2
John Deng <john.deng@qq.com>
2021-03-07 22:55:30 +0800

    added example code

---
+++ examples/go-app/go.mod
@@ -0,0 +1,5 @@
+module github.com/cloudnativeapps/docker-builder/examples/go-app
+
+go 1.15
+
+require github.com/cloudnativeapps/private-demo-api v1.0.0
---
+++ examples/go-app/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/cloudnativeapps/private-demo-api/pkg/foo"
+)
+
+func main() {
+	fmt.Println(foo.Message)
+}
